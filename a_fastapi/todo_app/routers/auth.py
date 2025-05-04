5d1a4ef481d01d1d070512fa46bcdc64243358d9
NahumFGz <ngutierrez2507@gmail.com>
2025-05-04 18:58:57 -0500

    feat: :sparkles: Agregando la creación de usuarios

--- a_fastapi/todo_app/routers/auth.py
+++ a_fastapi/todo_app/routers/auth.py
@@ -1,8 +1,65 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-router = APIRouter(prefix="/auth", tags=["auth"])
+from database import SessionLocal
+from fastapi import APIRouter, Depends, status
+from models import User
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+router = APIRouter(tags=["Auth"])
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-@router.get("/auth/")
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+    class Config:
+        from_attributes = True
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.get("/")
 async def get_user():
     return {"user": "authenticated"}
+
+
+@router.get("/all", status_code=status.HTTP_200_OK)
+async def get_all_users(db: db_dependency):
+    users = db.query(User).all()
+    return users
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_model = User(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True,
+    )
+
+    db.add(create_user_model)
+    db.commit()
+    db.refresh(create_user_model)
+
+    return create_user_model
